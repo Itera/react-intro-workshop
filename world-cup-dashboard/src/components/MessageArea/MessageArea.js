b5dfd3e3533ee902f32a4314b646451c40fbb6e8
Kristoffer Adam Tronerud <kristoffer.adam.tronerud@itera.no>
2018-06-29 17:00:05 +0200

    feat: Create component <MessageArea />
    Calls onSendMessage with text inputted along with author "Send" is hit.
    Probably one of the more complex components.

---
+++ world-cup-dashboard/src/components/MessageArea/MessageArea.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Button from '../Button/Button';
+import './MessageArea.css';
+
+class MessageArea extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: '',
+    }
+  }
+
+  handleOnSendMessage = () => {
+    this.props.onSendMessage({ author: this.props.author, text: this.state.text });
+    this.setState({ text: '' });
+  };
+
+  handleOnChange = (event) => this.setState({ text: event.target.value });
+
+  render() {
+    return (
+      <div className='message-area'>
+        Send a message as <strong>{this.props.author}</strong>
+        <textarea value={this.state.text} onChange={this.handleOnChange} />
+        <Button onClick={this.handleOnSendMessage} name='Send' />
+      </div>
+    );
+  }
+}
+
+MessageArea.propTypes = {
+  author: PropTypes.string.isRequired,
+  onSendMessage: PropTypes.func.isRequired,
+};
+
+export default MessageArea;
