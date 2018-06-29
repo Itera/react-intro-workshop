929882d4c7f61e06057f4a43ff7e9617a52bb90d
Kristoffer Adam Tronerud <kristoffer.adam.tronerud@itera.no>
2018-06-29 16:53:45 +0200

    feat: Create component <GoalCounter />
    Increase or decrease number of goals of a team using state.

---
+++ world-cup-dashboard/src/components/GoalCounter/GoalCounter.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Button from '../Button/Button';
+import './GoalCounter.css';
+
+class GoalCounter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      counter: 0,
+    }
+  }
+
+  increaseScore = () => this.setState({ counter: this.state.counter + 1 });
+
+  decreaseScore = () => this.setState({ counter: this.state.counter - 1 });
+
+  render() {
+    return (
+      <div className="goal-counter">
+        <Button onClick={this.decreaseScore} name="-" />
+        {this.props.team}: {this.state.counter}
+        <Button onClick={this.increaseScore} name="+" />
+      </div>
+    )
+  }
+}
+
+GoalCounter.propTypes = {
+  team: PropTypes.string.isRequired,
+};
+
+export default GoalCounter;
