d4c44c8df30c97b55bf1cb77c7d9df32c23ac70e
Kristoffer Adam Tronerud <kristoffer.adam.tronerud@itera.no>
2018-07-02 20:02:24 +0200

    refactor: Using index as keys are an anti-pattern. Use proper id's instead

--- world-cup-dashboard/src/components/ChatBoard/ChatBoard.js
+++ world-cup-dashboard/src/components/ChatBoard/ChatBoard.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import shortid from 'shortid';
 import './ChatBoard.css';
 
 const ChatBoard = (props) => (
   <div className="chat-board">
     <strong>{props.title}</strong>
     <ul>
-      {props.messages.map((message, i) => (
-        <li key={i}>
+      {props.messages.map(message => (
+        <li key={shortid.generate()}>
           <strong>{message.author}: </strong>{message.text}
         </li>
       ))}
